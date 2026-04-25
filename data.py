b5d8bb03a30e6f4238027b4834a75c58884b9d8b
Sompalli Likhitha <140173845+likithachinnibunny@users.noreply.github.com>
2026-04-25 10:27:50 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,6 @@
+movie_db = {
+    "Action": ["Mad Max", "John Wick", "Avengers"],
+    "Comedy": ["The Mask", "Superbad", "Hangover"],
+    "Horror": ["Conjuring", "Insidious", "Nun"],
+    "Drama": ["Forrest Gump", "Titanic", "Pursuit of Happyness"]
+}
